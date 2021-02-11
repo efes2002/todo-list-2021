9a95e18aace3e481678981f256b5fd79c171693b
efes2002 <65197828+efes2002@users.noreply.github.com>
2021-02-11 18:01:59 +0300

    11-02-2021 18:01

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,16 +2,32 @@ import React from 'react'
 import './item-add-form.css'
 
 class ItemAddForm extends React.Component {
+    state = {
+        label: ''
+    };
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        })
+    };
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addItem(this.state.label);
+        this.setState({label: ''});
+    };
+
     render () {
-        const {addItem} = this.props;
         return (
-            <form className='item-add-form d-flex'>
+            <form className='item-add-form d-flex'
+                  onSubmit={this.onSubmit}>
                 <input type='text'
+                       id="id3"
                        className='form-control'
-                       onChange={this.onlabelChange}
-                       placeholder='Privet what you have'/>
+                       onChange={this.onLabelChange}
+                       placeholder='Privet what you have'
+                       value={this.state.label}/>
                 <button className='btn btn-outline-secondary'
-                        onClick={() => addItem("privet")}>Add Item</button>
+                        >Add Item</button>
             </form>
         )
     }
